0bdebcf52a7c5f163f7e390c88672c50555dc481
Jiovanni <Jkitlo2005@gmail.com>
2026-07-06 12:24:38 +0300

    chore: complete rebase and resolve conflicts

--- src/middleware/validateTodo.middleware.ts
+++ src/middleware/validateTodo.middleware.ts
@@ -4,11 +4,7 @@ export const validation = ( req:Request, res:Response, next:NextFunction ) => {
 
     const { title , completed }= req.body;
     if ( !title ){
-<<<<<<< HEAD
         return res.status(400).json({
-=======
-       return res.status(400).json({
->>>>>>> 0712e54 (fix: guard todo creation and align tests with userId requirement)
             message : "Title is Required!! "
         });
     }

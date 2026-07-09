dbc3126055d4c7fd4db4dfcaeec5660a02743333
Jiovanni <Jkitlo2005@gmail.com>
2026-07-09 08:50:48 +0300

    fix: add JWT_EXPIRES_IN in ci.yml

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,21 +1,20 @@
-import jwt , {SignOptions} from "jsonwebtoken";
+import jwt, { SignOptions } from "jsonwebtoken";
 
-export interface JwtPayload{
+export interface JwtPayload {
     id: string;
     email: string;
     role: string;
-};
+}
 
-const getSecret= (): string =>{
-    const secret= process.env.JWT_SECRET;
-    if(!secret){
-        throw new Error("JWT_SECRET is not defined in environment variables");
+const getSecret = (): string => {
+    const secret = process.env.JWT_SECRET;
+    if (!secret) {
+        throw new Error("JWT_SECRET is not set.");
     }
     return secret;
-}
+};
 
-export const signToken= (payload: JwtPayload): string =>{
-    const expiresIn = (process.env.JWT_EXPIRES_IN) as SignOptions["expiresIn"];
-    return jwt.sign(payload,getSecret(), {expiresIn});
-
-}
+export const signToken = (payload: JwtPayload): string => {
+    const expiresIn = (process.env.JWT_EXPIRES_IN ?? "1h") as SignOptions["expiresIn"];
+    return jwt.sign(payload, getSecret(), { expiresIn });
+};
